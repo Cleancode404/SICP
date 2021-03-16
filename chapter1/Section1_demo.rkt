2632a874223c7baafe404d7f59719301994d2a1a
Heuristic <codemonkey2468@hotmail.com>
2021-03-16 15:16:07 -0400

    continue chapter 1

--- chapter1/Section1_demo.rkt
+++ chapter1/Section1_demo.rkt
@@ -68,3 +68,37 @@
         ((< x 0) (- x))))
 (abs -11) ;output should be 11
 
+;1.1.7 Example: square roots by Newton's method
+(define (sqrt x)
+  (the y (and (>= y 0)
+              (= (square y) x))))
+
+
+;Newton's method
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve guess x ) x)))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (good-enough? guess x)
+(< (abs (- (square guess) x)) 0.001))
+
+(define (sqrt x )
+  (sqrt-iter 1.0 x))
+
+(define (square x) ;miss this in the book but previous section has it
+  (* x x))
+
+
+(sqrt 9)
+(sqrt (+ 100 37))
+
+(sqrt (+ (sqrt 2) (sqrt 3)))
+
+(square (sqrt 1000))
