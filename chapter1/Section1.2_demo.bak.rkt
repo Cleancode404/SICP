81a2bafcd9bd6ce5e93019922a574223486233f4
Heuristic <codemonkey2468@hotmail.com>
2021-04-26 12:01:48 -0400

    continue section 1.2

--- chapter1/Section1.2_demo.bak.rkt
+++ chapter1/Section1.2_demo.bak.rkt
@@ -1,7 +1,5 @@
 #lang racket
 ;Section 1.2 Procedures and the Process they generate
-
-
 (define (factorial n)
   (if (= n 1)
       1
@@ -10,3 +8,17 @@
 ;linear recursive process for computing 6!
 (factorial 6)
 
+
+
+
+;another approach with linear iterative process
+(define (factorial n)
+  (fact-iter 1 1 n))
+(define (fact-iter product counter max-count)
+  (if (> counter max-count)
+      product
+      (fact-iter (* counter product)
+                 (+ counter 1)
+                 max-count)))
+
+(factorial 6)
