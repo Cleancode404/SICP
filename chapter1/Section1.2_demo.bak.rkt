7969012bf37d6f4813c750837eb5bd4859e48026
Heuristic <codemonkey2468@hotmail.com>
2021-04-26 14:20:44 -0400

    continue section 1.2

--- chapter1/Section1.2_demo.bak.rkt
+++ chapter1/Section1.2_demo.bak.rkt
@@ -30,3 +30,14 @@
         ((= n 1) 1)
         (else (+ (fib (- n 1))
                  (fib (- n 2))))))
+
+
+
+;another fibonacc approach
+(define (fib n)
+  (fib-iter 1 0 n))
+
+(define (fib-iter a b count)
+  (if (= count 0)
+      b
+      (fib-iter (+ a b) a (- count 1 ))))
