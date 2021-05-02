93153884646c533affd6e3048468f0bd89cf589d
Heuristic <codemonkey2468@hotmail.com>
2021-05-02 17:04:57 -0400

    continue section 1.2

--- chapter1/Section1.2_demo.bak.rkt
+++ chapter1/Section1.2_demo.bak.rkt
@@ -83,3 +83,15 @@
   (if (= n 0)
       1
       (* b (expt b (- n 1)))))
+
+(define (expts b n)
+  (expt-iter b n 1))
+
+(define (expt-iter b counter product)
+  (if (= counter 0 )
+      product
+      (expt-iter b
+                 (- counter 1)
+                 (* b product))))
+
+(expts 2 5)
