159df8c60853ad7ab006291090b7321074f1aadf
Heuristic <codemonkey2468@hotmail.com>
2021-05-03 15:23:29 -0400

    cont exercise 7

--- chapter1/Exercise1.7.racket.rkt
+++ chapter1/Exercise1.7.racket.rkt
@@ -2,3 +2,17 @@
 ;trauncated error
 ;for example: 1000 - 0.001 is approximate to be 1000 because 0.001 is too small
 
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.1))
+
+(define (small-enough guess x)
+  (<=
+   (diff (sqr guess) x)
+   (diff (sqr (improve guess x)) x)))
+
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+      (if (small-enough guess x)
+          guess
+          (sqrt-iter (improve guess x) x))
+       (sqrt-iter (improve guess x))))
