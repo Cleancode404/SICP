cecea6188b466ed70005e58d9b97bca92a909ad0
Heuristic <codemonkey2468@hotmail.com>
2021-05-10 13:07:01 -0400

    cont exercise 11

--- chapter1/Exercise.1.11.racket.rkt
+++ chapter1/Exercise.1.11.racket.rkt
@@ -1,12 +1,16 @@
 #lang racket
 ;exercise 11
+
 ;recursive process
-(define (f n)
+(define (f_recursive n)
   (if (< n 3)
       n
-      (+ (- n 1) (* 2(- n 2)) (* 3(- n 3))) 
+      (+ (- n 1) (* 2(- n 2)) (* 3(- n 3))))) 
 
-(f 2)
+;check output
+(f_recursive 3)
+(f_recursive 4)
 
 
 ;iterative process
+(
