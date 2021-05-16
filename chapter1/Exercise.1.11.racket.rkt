56512720a62b952a4db3d4f369e4d3f734b40d81
Heuristic <codemonkey2468@hotmail.com>
2021-05-16 16:57:57 -0400

    finish exercise 11

--- chapter1/Exercise.1.11.racket.rkt
+++ chapter1/Exercise.1.11.racket.rkt
@@ -5,13 +5,26 @@
 (define (f_recursive n)
   (if (< n 3)
       n
-      (+ (- n 1) (* 2(- n 2)) (* 3(- n 3))))) 
+      (+ (f_recursive (- n 1))
+         (* 2 (f_recursive(- n 2)))
+         (* 3(f_recursive (- n 3))))) 
 
 ;check output
-(f_recursive 3)
+;(f_recursive 3)
 (f_recursive 4)
 
 
 ;iterative process
-;(define (f_iterative n)
-  
+(define (f_iterative n)
+  (f_iter 2 1 0 0 n))
+
+(define (f_iter a b c i n)
+  (if (= i n)
+      c
+      (f_iter (+ a (* 2 b) (* 3 c)) ; new a
+                 a                 ; new b
+                 b                 ; new c
+                 (+ i 1)
+                 n)))
+
+(f_iter 2 1 0 0 4)
