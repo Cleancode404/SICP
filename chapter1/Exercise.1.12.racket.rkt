f9bed299aee149f21a8d5d9b4e78e2d216556a6b
Heuristic <codemonkey2468@hotmail.com>
2021-05-17 12:07:07 -0400

    finish exercise 12

--- chapter1/Exercise.1.12.racket.rkt
+++ chapter1/Exercise.1.12.racket.rkt
@@ -1,2 +1,15 @@
 #lang racket
 ;exericse 1.12
+
+;write a procedure that computes elements of Pascal's triangle by means of a recursive process
+
+;approach with row and column. Both start counting from zero
+
+(define (pascal row col)
+  (cond ((> col row)
+         (error "invalid column value"))
+        ((or (= col 0) (= row col))
+         1)
+        (else (+ (pascal (- row 1) (- col 1))
+                 (pascal (- row 1) col)))))
+
