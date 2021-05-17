909f97812799c8a0b9c13268f95fcbedf3e1702d
Heuristic <codemonkey2468@hotmail.com>
2021-05-17 12:16:18 -0400

    finish exercise 13

--- chapter1/Exercise.1.13.racket.rkt
+++ chapter1/Exercise.1.13.racket.rkt
@@ -1,2 +1,16 @@
 #lang racket
 ;exercise 1.13
+
+;this is a math proof problem and this cannot type math equation
+;so cant do it here...
+
+;approach
+;Fib(n) is given
+
+;Find out Fib(n + 1) and Fib(n + 2), plug in theta is (1 - sqrt5)/2
+
+;since Fib(n + 2) = Fib(n + 1) + Fib(n)
+
+;after subsitution you see Fib(n) given is correct
+
+;therefore Fib(n) is the closest integer to theta^n/sqrt(5)
